3bcf7d52a20c44672e4c2038add379d98d0cd61c
YaranN <y4nan@uwaterloo.ca>
2018-10-01 15:24:26 -0400

    Initial commit

---
+++ app/src/main/java/com/example/yaran/firstapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.yaran.firstapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void sign_up_view(View view){
+    /*    Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);*/
+
+    }
+}
