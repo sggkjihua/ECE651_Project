8572da9e5393a951aac0598288fcd91e953d1777
YaranN <y4nan@uwaterloo.ca>
2018-10-06 12:28:03 -0400

    1. Added databaseHelper class 2. check register information 3. Fixed some format issue

--- app/src/main/java/com/example/yaran/firstapp/MainActivity.java
+++ app/src/main/java/com/example/yaran/firstapp/MainActivity.java
@@ -14,11 +14,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void sign_up_view(View view){
-    /*    Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);*/
+        Intent intent = new Intent(this, register_activity.class);
+        startActivity(intent);
 
     }
 }
---
+++ app/src/main/java/com/example/yaran/firstapp/databaseHelper.java
@@ -0,0 +1,45 @@
+package com.example.yaran.firstapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class databaseHelper extends SQLiteOpenHelper {
+    public databaseHelper(Context context) {
+        super(context, "Login.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("Create table user(email text primary key,password text)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists user");
+    }
+    //Insert
+    public boolean insert(String email, String password)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("email",email);
+        //contentValues.put("name",name);
+        contentValues.put("password",password);
+        long ins = db.insert("user",null, contentValues);
+        if (ins == -1) return false;
+        else return true;
+    }
+    public boolean check_email(String email){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("Select * from user where email=?",new String[]{email});
+        if(cursor.getCount()>0) return false;
+        else {
+            return true;
+        }
+
+    }
+
+}
